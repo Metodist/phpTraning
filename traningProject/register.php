3508b4b33e37cf804fe04176e96a10253422eea4
Iona M <51836963+Metodist@users.noreply.github.com>
2021-08-11 05:06:40 +0300

    домашка по 1 заданию - погружение

--- traningProject/register.php
+++ traningProject/register.php
@@ -11,13 +11,25 @@ $statement->execute(['email'=> $email]);
 $item = $statement->fetchAll(PDO::FETCH_ASSOC);
 //var_dump($item);
 
+function user_register(string $email, string $pass){
+    $pdo = new PDO("mysql:host=127.0.0.1; dbname=phptraning;", "root", "root");
+    $sql = "INSERT INTO users (email, password) VALUES ('$email', '$pass')";
+    $statement = $pdo->prepare($sql);
+    $statement->execute();
+    $a = $statement->fetchAll(PDO::FETCH_ASSOC);
+}
+
 if(!empty($item)){
     $message = "<strong>Уведомление!</strong> Этот эл. адрес уже занят другим пользователем.";
     $_SESSION['danger'] = $message;
     header("Location: /traningProject/page_register.php");
     exit;
 }else{
-    echo 'регистрация возможна';
+    user_register($email, $pass);
+    $message = "Регистрация успешна";
+    $_SESSION['success'] = $message;
+    header("Location: /traningProject/page_login.php");
+    exit;
 }
 
 
